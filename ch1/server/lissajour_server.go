9e2edd2d7966a2e57ba58e2f4f7af2ad8a6aa4a8
gezi-lzq <lzqtxwd@gamil.com>
2022-06-20 13:30:42 +0800

    updata lissajour_server

--- ch1/server/lissajour_server.go
+++ ch1/server/lissajour_server.go
@@ -1,6 +1,8 @@
 // Lissajous generates GIF animations of random Lissajous figures.
 package main
 
+// 练习 1.12
+// 修改Lissajour服务，从URL读取变量，比如你可以访问 http://localhost:8000/?cycles=20 这个URL，这样访问可以将程序里的cycles默认的5修改为20。
 import (
 	"image"
 	"image/color"
